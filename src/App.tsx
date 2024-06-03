e94f963626970ec488bc44cb8cf23f4eff939009
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-03 22:52:19 +0600

    color-changer chrome extension added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('#ffffff');
+
+  async function clickHandler() {
+    const [tab] = await chrome.tabs.query({ active: true });
+
+    if (!tab) return;
+
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id! },
+      args: [color],
+      func: (color) => {
+        document.body.style.backgroundColor = color;
+      },
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h3>BG COLOR CHANGER</h3>
+      <div className="inner">
+        <input
+          defaultValue={color}
+          value={color}
+          type="color"
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button onClick={() => clickHandler()}>Change</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
